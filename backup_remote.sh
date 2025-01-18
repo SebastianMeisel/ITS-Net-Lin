b77ace8f3726b85fafecf7850915152a20cb16e3
Sebastian Meisel <sebastian.meisel@gmail.com>
2025-01-18 17:37:47 +0100

    Remote Backupskript

--- backup_remote.sh
+++ backup_remote.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 SUB_DIR="bros"
 BACKUP_DIR="/backup"
-# Für remote Backups, z.B. BACKUP_DIR="deb:/backup"
 SOURCE_DIR="/home/${SUB_DIR}"
+SERVER="deb"
 
 # Datum im Format YYYYMMDD
 DATE=$(date +%Y%m%d)
@@ -15,28 +15,28 @@ LASTB="${BACKUP_DIR}/Latest"
 # Backup-Art je nach Argument
 case "$1" in
   full)
-    # Stelle sicher, dass Zielverzeichnis existiert 
-    [[ -d ${FULLB} ]] || mkdir -p -m 777 ${FULLB}
+    # Stelle sicher, dass Zielverzeichnis auf dem Server existiert 
+    ssh ${SERVER} "[[ -d ${FULLB} ]] || mkdir -p -m 777 ${FULLB}"
     # Synchronisiere
-    rsync -av ${SOURCE_DIR} ${FULLB}
+    rsync -av ${SOURCE_DIR} ${SERVER}:${FULLB}
     # Link to Latest
-    ln -snf ${FULLB} ${LASTB}
+    ssh ${SERVER} "ln -snf ${FULLB} ${LASTB}"
     ;;
   incremental)
     # Stelle sicher, dass Zielverzeichnis existiert 
-    [[ -d ${INCRB} ]] || mkdir -p -m 777 ${INCRB}
+    ssh ${SERVER} "[[ -d ${INCRB} ]] || mkdir -p -m 777 ${INCRB}"
     # Synchronisiere
-    rsync -av --link-dest=../../Latest ${SOURCE_DIR} ${INCRB}
+    rsync -av --link-dest=../../Latest ${SOURCE_DIR} ${SERVER}:${INCRB}
     # Link to Latest
-    ln -snf ${INCRB} ${LASTB}
+    ssh ${SERVER} "ln -snf ${INCRB} ${LASTB}"
     ;;
   differential)
     # Stelle sicher, dass Zielverzeichnis existiert 
-    [[ -d ${DIFFB} ]] || mkdir -p -m 777 ${DIFFB}
+    ssh ${SERVER} "[[ -d ${DIFFB} ]] || mkdir -p -m 777 ${DIFFB}"
     # Synchronisiere
-    rsync -av --link-dest=../../Vollbackup ${SOURCE_DIR} ${DIFFB}
+    rsync -av --link-dest=../../Vollbackup ${SOURCE_DIR} ${SERVER}:${DIFFB}
     # Link to Latest
-    ln -snf ${DIFFB} ${LASTB}
+    ssh ${SERVER} "ln -snf ${DIFFB} ${LASTB}"
     ;;
   *)
     echo "Usage: $0 {full|differential|incremental}"
